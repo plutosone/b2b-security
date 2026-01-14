52c11a832c40587ba517783e8b6716f8cc0774e2
Gaurav Yadav <gaurav@plutos.one>
2026-01-14 14:17:54 +0530

    url correction

--- src/main/java/org/plutos/one/security/ComplaintResource.java
+++ src/main/java/org/plutos/one/security/ComplaintResource.java
@@ -14,7 +14,7 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-@Path("ibmb")
+@Path("b2bsecurity")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ComplaintResource {
